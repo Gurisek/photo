4c95aeb18792d6e98f977c3475e0b43a5552f8be
Roman Dušek <romik.dusek@seznam.cz>
2023-10-21 12:59:39 +0200

    .

--- src/Pages/Portfolio/Weddings.js
+++ src/Pages/Portfolio/Weddings.js
@@ -5,20 +5,21 @@ import { Link } from "react-router-dom";
 export default function Weddings() {
   return (
     <div className="Weddings">
-      <h1>Weddings</h1>
+      <h1>Svatby</h1>
       <div className="gallery_weddings">
-      <Link to="/weddings/1b47h">
+        <div className="galerry_item">
+          <Link to="/weddings/1b47h">
             <img src={pic1} alt="1" />
-
             <h2>Manželé 1</h2>
           </Link>
-        <div className="gallery_item">
-          <img src={pic1} alt="1" />
-          <h2>Manželé 2</h2>
         </div>
         <div className="gallery_item">
-
+          <Link to="/weddings/1b47h">
+            <img src={pic1} alt="1" />
+            <h2>Manželé 2</h2>
+          </Link>
         </div>
+        <div className="gallery_item"></div>
       </div>
     </div>
   );
