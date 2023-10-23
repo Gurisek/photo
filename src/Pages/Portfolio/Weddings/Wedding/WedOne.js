f84578ceeca26df900d62a788f233dbadc31f83a
Roman Dušek <romik.dusek@seznam.cz>
2023-10-23 11:04:03 +0200

    .

--- src/Pages/Portfolio/Weddings/Wedding/WedOne.js
+++ src/Pages/Portfolio/Weddings/Wedding/WedOne.js
@@ -2,30 +2,8 @@ import "../../css/WedOne.css";
 import React, { useState } from "react";
 import data_1 from "../data/data_1";
 
-// export const WedOne = () => {
-//   return (
-//     <div className="">
-//       <h1>Wedding 1</h1>
-//       <div className="">
-        
-//         {data_1.map((onePic) => {
-//           const { id, img } = onePic;
-//           return (
-//             <div className="pic">
-//               <img src={img} alt={id} />
-//             </div>
-//           );
-//         })}
-//       </div>
-//     </div>
-//   );
-// };
-
 export const WedOne = () => {
-
   const [selectedImage, setSelectedImage] = useState(null);
-
-
 
   const openImage = (onePic) => {
     setSelectedImage(onePic);
@@ -37,20 +15,28 @@ export const WedOne = () => {
 
   return (
     <div className="gallery">
-      {data_1.map((onePic) => {
+      <div className="wed_item">
+        {data_1.map((onePic) => {
           const { id, img } = onePic;
           return (
-        <img
-          key={id}
-          src={img}
-          alt={id}
-          onClick={() => openImage(onePic)}
-        />
-          )})}
+            <img
+              key={id}
+              src={img}
+              alt={id}
+              onClick={() => openImage(onePic)}
+              loading="lazy"
+            />
+          );
+        })}
+      </div>
 
       {selectedImage && (
         <div className="fullscreen-overlay" onClick={closeImage}>
-          <img src={selectedImage.img} alt={selectedImage.img} id="fullscreen-image" />
+          <img
+            src={selectedImage.img}
+            alt={selectedImage.img}
+            id="fullscreen-image"
+          />
           <span className="close-button">&times;</span>
         </div>
       )}
