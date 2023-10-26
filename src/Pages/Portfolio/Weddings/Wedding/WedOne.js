5c96846124d28bc1b6de93412a575beccb4e47f1
Roman Dušek <romik.dusek@seznam.cz>
2023-10-27 00:29:06 +0200

    lazy

--- src/Pages/Portfolio/Weddings/Wedding/WedOne.js
+++ src/Pages/Portfolio/Weddings/Wedding/WedOne.js
@@ -1,9 +1,15 @@
 import "../../css/WedOne.css";
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense, useRef, useEffect, useCallback } from "react";
 import data_1 from "../data/data_1";
+
+const LazyImage = lazy(() => import("../../../../Hooks/LazyImage"));
 
 export const WedOne = () => {
   const [selectedImage, setSelectedImage] = useState(null);
+  const [startIndex, setStartIndex] = useState(0);
+  const [endIndex, setEndIndex] = useState(6);
+  const [loadedImages, setLoadedImages] = useState([]);
+  const galleryRef = useRef(null);
 
   const openImage = (onePic) => {
     setSelectedImage(onePic);
@@ -13,19 +19,52 @@ export const WedOne = () => {
     setSelectedImage(null);
   };
 
+  const handleIntersection = useCallback((entries) => {
+    const lastEntry = entries[entries.length - 1];
+    if (lastEntry.isIntersecting && endIndex < data_1.length) {
+      setStartIndex(endIndex);
+      setEndIndex(endIndex + 6);
+    }
+  }, [endIndex]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(handleIntersection, {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.1,
+    });
+
+    const currentGalleryRef = galleryRef.current;
+
+    if (currentGalleryRef) {
+      observer.observe(currentGalleryRef);
+    }
+
+    return () => {
+      if (currentGalleryRef) {
+        observer.unobserve(currentGalleryRef);
+      }
+    };
+  }, [handleIntersection]);
+
+  const loadImages = useCallback((start, end) => {
+    const imagesToLoad = data_1.slice(start, end);
+    setLoadedImages((prevLoadedImages) => [...prevLoadedImages, ...imagesToLoad]);
+  }, []);
+
+  useEffect(() => {
+    loadImages(startIndex, endIndex);
+  }, [startIndex, endIndex, loadImages]);
+
   return (
-    <div className="gallery">
+    <div className="gallery" ref={galleryRef}>
       <div className="wed_item">
-        {data_1.map((onePic) => {
+        {loadedImages.map((onePic) => {
           const { id, img } = onePic;
           return (
-            <img
-              key={id}
-              src={img}
-              alt={id}
-              onClick={() => openImage(onePic)}
-              loading="lazy"
-            />
+            <Suspense key={id} fallback={<div>Loading...</div>}>
+              <LazyImage src={img} alt={id} onClick={() => openImage(onePic)} />
+            </Suspense>
           );
         })}
       </div>
