0e14b8149afbbcf492e00a7a65e4d04a63c00225
Roman Dušek <romik.dusek@seznam.cz>
2023-12-04 11:06:36 +0100

    upraveno nacitani fotek, uprava urls

--- src/Pages/Portfolio/Weddings/Wedding/Wed3.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ImageGallery from "./ImageGallery";
-import wedding3 from "./imageList/wedding3";
-
-export const Wed3 = () => {
-  return <ImageGallery data={wedding3} />;
-};
